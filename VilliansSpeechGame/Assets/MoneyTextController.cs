c95f8f6ba91b9783c6ba6c3f3be892f435d470b8
Unknown <smithja@ferris.edu>
2018-01-27 03:32:45 -0500

    HentchmanUIDOne
    HentchmenUI is Finished

---
+++ VilliansSpeechGame/Assets/MoneyTextController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class MoneyTextController : MonoBehaviour {
+    public int totalMoney;
+    public Text moneyText;
+	// Use this for initialization
+	void Start () {
+
+        totalMoney = 50;
+
+        moneyText = GetComponentInParent<Text>();
+        moneyText.text =  totalMoney.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moneyText.text = totalMoney.ToString();
+    }
+}
