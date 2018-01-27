9c9c4ca0d7bbeb7972cc5c21780c8c7a615497c3
SweaterWeatherGames <resonance.data@gmail.com>
2018-01-27 18:08:19 -0500

    started work on plugging my algorythms into andrew's menus

--- VilliansSpeechGame/Assets/Scripts/SchemeExecuter.cs
+++ VilliansSpeechGame/Assets/Scripts/SchemeExecuter.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class SchemeExecuter : MonoBehaviour {
 
     Result result;
+    public BoxController methodBox;
+    public BoxController demandBox;
+    public BoxController threatBox;
+    public BoxController locationBox;
+    public BoxController bluffBox;
 
 	public Result calculateSuccess(Plan plan, DistributionMethod dist, bool bluff = false)
     {
@@ -21,6 +26,39 @@ public class SchemeExecuter : MonoBehaviour {
         StatManager.Instance.terrorizeZone(plan.targetZone, plan.hysteriaProvided);
         StatManager.Instance.policeProgress += (StatManager.Instance.thugs * 20 + StatManager.Instance.robbers * 10 - StatManager.Instance.pols * 10);
         return new Result();//enacted plan
+
+        
+    }
+
+    public void generatePlan() {
+        Plan plan = new Plan();
+        DistributionMethod dist = new DistributionMethod();
+        bool bluff = (bluffBox.value == 1);
+
+        dist.successMod = methodBox.value;
+        dist.evidenceProduced = (methodBox.value - 1) * 10;
+
+        plan.hysteriaNeeded = (demandBox.value - 1) * 100;
+
+        switch (threatBox.value)
+        {
+            //kidnapping
+            case 1:
+                plan.hysteriaProvided = 25;
+                break;
+            //poison
+            case 2:
+                plan.hysteriaProvided = 50;
+                break;
+            //demolitions
+            case 3:
+                plan.hysteriaProvided = 100;
+                break;
+        }
+
+        plan.targetZone = locationBox.value - 1;
+
+
     }
 
     public bool calculateLoseState()
