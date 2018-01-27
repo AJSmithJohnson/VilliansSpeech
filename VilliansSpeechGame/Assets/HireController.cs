65b758e934dab81d64b93a6b0048f2ab7c9ea77a
Unknown <smithja@ferris.edu>
2018-01-27 03:32:45 -0500

    HentchmanUIDOne
    HentchmenUI is Finished

---
+++ VilliansSpeechGame/Assets/HireController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HireController : MonoBehaviour {
+    public MoneyTextController mtController;
+    public int minionType;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SpendMoney()
+    {
+        if(minionType == 1)
+        {
+            mtController.totalMoney -= 5;
+        }
+        else if (minionType == 2)
+        {
+            mtController.totalMoney -= 15;
+        }
+        else if (minionType == 3)
+        {
+            mtController.totalMoney -= 50;
+        }
+    }
+}
